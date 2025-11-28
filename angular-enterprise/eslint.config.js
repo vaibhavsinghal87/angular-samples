a98d2982bc8f3eb0b6ff666e8528a48928283b27
VS <vaibhavsinghal87@gmail.com>
2025-11-28 14:18:16 +0530

    ESLint setup

--- angular-enterprise/eslint.config.js
+++ angular-enterprise/eslint.config.js
@@ -1,176 +1,153 @@
-import js from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+// @ts-check
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const angularTemplate = require('@angular-eslint/eslint-plugin-template');
+const angularParser = require('@angular-eslint/template-parser');
+const tsParser = require('@typescript-eslint/parser');
+const globals = require('globals');
 
-import angularEslint from '@angular-eslint/eslint-plugin';
-import importPlugin from 'eslint-plugin-import';
-import prettier from 'eslint-plugin-prettier';
-import promisePlugin from 'eslint-plugin-promise';
-import sonarjs from 'eslint-plugin-sonarjs';
+// import { defineConfig } from 'eslint/config';
+// import { ESLint } from 'eslint';
+// import globals from 'globals';
+// import angularTemplate from '@angular-eslint/eslint-plugin-template';
+// import angularParser from '@angular-eslint/template-parser';
+// import tseslint from '@typescript-eslint/eslint-plugin';
+// import tsParser from '@typescript-eslint/parser';
+// import angular from 'angular-eslint';
 
-export default defineConfig([
-  // JavaScript configuration
+module.exports = defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs}'],
-    plugins: {
-      js,
-      prettier,
-      import: importPlugin,
-      promise: promisePlugin,
-      sonarjs,
-      angular: angularEslint,
-    },
-    extends: [
-      'js/recommended',
-      'plugin:prettier/recommended',
-      'plugin:import/recommended',
-      'plugin:promise/recommended',
-      'plugin:sonarjs/recommended'
-    ],
+    files: ['**/*.ts'],
     languageOptions: {
+      parser: tsParser,
+      // parserOptions: {
+      //   project: './tsconfig.json',
+      //   sourceType: 'module',
+      // },
       globals: {
         ...globals.browser,
-        ...globals.es2021
-      }
-    },
-    settings: {
-      'import/resolver': {
-        node: {
-          extensions: ['.js', '.jsx', '.ts', '.tsx']
-        }
-      }
-    },
-    rules: {
-      // Prettier Integration
-      'prettier/prettier': ['error', {
-        'singleQuote': true,
-        'trailingComma': 'es5',
-        'printWidth': 100,
-        'semi': true,
-        'bracketSpacing': true
-      }],
-
-      // Import Plugin Rules
-      'import/order': ['error', {
-        'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        'newlines-between': 'always',
-        'alphabetize': { 'order': 'asc', 'caseInsensitive': true }
-      }],
-      'import/no-unresolved': 'error',
-      'import/no-cycle': 'error',
-
-      // Promise Plugin Rules
-      'promise/always-return': 'error',
-      'promise/no-return-wrap': 'error',
-      'promise/param-names': 'error',
-
-      // SonarJS Rules
-      'sonarjs/cognitive-complexity': ['error', 15],
-      'sonarjs/no-duplicate-string': 'warn',
-      'sonarjs/no-identical-functions': 'warn',
-      // Error Prevention
-      'no-console': 'warn',
-      'no-debugger': 'warn',
-      'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
-      'no-undef': 'error',
-      'no-var': 'error',
-      'prefer-const': 'error',
-
-      // Best Practices
-      'eqeqeq': ['error', 'always'],
-      'no-eval': 'error',
-      'no-implied-eval': 'error',
-      'no-return-await': 'error',
-      'require-await': 'error',
-
-      // Style
-      'arrow-body-style': ['error', 'as-needed'],
-      'arrow-parens': ['error', 'as-needed'],
-      'comma-dangle': ['error', 'always-multiline'],
-      'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
-      'semi': ['error', 'always'],
-
-      // ES6+
-      'prefer-arrow-callback': 'error',
-      'prefer-template': 'error',
-      'no-duplicate-imports': 'error'
-    }
-  },
-  // TypeScript configuration
-  {
-    files: ['**/*.{ts,mts,cts}'],
-    plugins: {
-      prettier,
-      import: importPlugin,
-      promise: promisePlugin,
-      sonarjs,
-      angular: angularEslint,
-    },
-    extends: [
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.strictTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-      'plugin:prettier/recommended',
-      'plugin:import/typescript',
-      'plugin:promise/recommended',
-      'plugin:sonarjs/recommended',
-      'plugin:@angular-eslint/recommended',
-      'plugin:@angular-eslint/template/process-inline-templates'
-    ],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.es2021
       },
-      parserOptions: {
-        project: true
-      }
     },
+    plugins: {},
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
     rules: {
-      // TypeScript Specific
-      '@typescript-eslint/explicit-function-return-type': ['error', {
-        'allowExpressions': true,
-        'allowTypedFunctionExpressions': true
-      }],
-      '@typescript-eslint/explicit-member-accessibility': ['error', {
-        'accessibility': 'explicit',
-        'overrides': {
-          'constructors': 'no-public'
-        }
-      }],
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-non-null-assertion': 'error',
-      '@typescript-eslint/no-unused-vars': ['warn', {
-        'argsIgnorePattern': '^_',
-        'varsIgnorePattern': '^_'
-      }],
-      '@typescript-eslint/prefer-optional-chain': 'error',
-      '@typescript-eslint/strict-boolean-expressions': 'error',
-
-      // Style
-      '@typescript-eslint/sorted-imports': ['error', {
-        'ignoreCase': true,
-        'ignoreDeclarationSort': true,
-        'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single']
-      }],
-      '@typescript-eslint/member-ordering': 'error',
-      '@typescript-eslint/naming-convention': [
+      // 'sort-imports': [
+      //   'error',
+      //   {
+      //     ignoreCase: false,
+      //     ignoreDeclarationSort: false,
+      //     ignoreMemberSort: false,
+      //     memberSyntaxSortOrder: [
+      //       'none',
+      //       'all',
+      //       'multiple',
+      //       'single',
+      //     ],
+      //     allowSeparatedGroups: false,
+      //   },
+      // ],
+      '@typescript-eslint/adjacent-overload-signatures':
+        'error',
+      '@typescript-eslint/no-inferrable-type': 'off',
+      '@angular-eslint/directive-selector': [
         'error',
         {
-          'selector': 'interface',
-          'format': ['PascalCase'],
-          'prefix': ['I']
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
         },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          'selector': 'typeAlias',
-          'format': ['PascalCase']
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
         },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    languageOptions: {
+      parser: angularParser,
+    },
+    plugins: {
+      // '@angular-eslint/template': angularTemplate,
+    },
+    extends: [
+      angular.configs.templateRecommended,
+      angular.configs.templateAccessibility,
+      // angularTemplate.configs.recommended,
+    ],
+    rules: {
+      ...angularTemplate.configs.recommended.rules,
+      '@angular-eslint/template/attributes-order': [
+        'warn',
         {
-          'selector': 'enum',
-          'format': ['PascalCase']
-        }
-      ]
-    }
-  }
+          alphabetical: true,
+        },
+      ],
+      '@angular-eslint/template/banana-in-box': ['error'],
+      '@angular-eslint/template/button-has-type': ['error'],
+      '@angular-eslint/template/click-events-have-key-events':
+        ['error'],
+      '@angular-eslint/template/conditional-complexity': [
+        'error',
+        {
+          maxComplexity: 5,
+        },
+      ],
+      '@angular-eslint/template/elements-content': [
+        'error',
+      ],
+      '@angular-eslint/template/eqeqeq': ['error'],
+      '@angular-eslint/template/no-any': ['error'],
+      '@angular-eslint/template/no-call-expression': [
+        'error',
+      ],
+      '@angular-eslint/template/no-duplicate-attributes': [
+        'error',
+      ],
+      '@angular-eslint/template/no-empty-control-flow': [
+        'error',
+      ],
+      '@angular-eslint/template/no-inline-styles': [
+        'error',
+      ],
+      '@angular-eslint/template/no-interpolation-in-attributes':
+        ['warn'],
+      '@angular-eslint/template/no-negated-async': [
+        'error',
+      ],
+      '@angular-eslint/template/no-nested-tags': ['error'],
+      '@angular-eslint/template/no-positive-tabindex': [
+        'error',
+      ],
+      '@angular-eslint/template/prefer-at-else': ['error'],
+      '@angular-eslint/template/prefer-at-empty': ['error'],
+      '@angular-eslint/template/prefer-built-in-pipes': [
+        'warn',
+      ],
+      '@angular-eslint/template/prefer-contextual-for-variables':
+        ['warn'],
+      '@angular-eslint/template/prefer-control-flow': [
+        'error',
+      ],
+      '@angular-eslint/template/prefer-ngsrc': ['error'],
+      '@angular-eslint/template/prefer-static-string-properties':
+        ['warn'],
+      '@angular-eslint/template/prefer-template-literal': [
+        'error',
+      ],
+    },
+  },
 ]);
